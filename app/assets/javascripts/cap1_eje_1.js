0a2c921fbd6ebcd6071d91a3fdff65604980949c
rakatongo <rakatongo@hotmail.com>
2012-11-10 12:50:40 -0300

    3rd capitulo

--- app/assets/javascripts/cap1_eje_1.js
+++ app/assets/javascripts/cap1_eje_1.js
@@ -1,1 +1,24 @@
-alert(document.getElementsByTagName('li').length);
+//alert(document.getElementsByTagName('l¡').length);
+$(document).ready(function(){	
+	// Parrafos:
+	// Loopeando por todos los parrafos elejidos, cambiando el titulo si no tienen.
+	var par = $('p');
+	for (var i = 0; i < par.length; i++) {
+		var titulo = par[i].getAttribute('title');
+		if (!titulo)
+			par[i].setAttribute('title', i + " Nuevo titulo") 
+		//alert(par[i].getAttribute('title'));
+	};	
+	
+	// Lista item:
+	var items = $('#compras li');
+	for (var i = 0; i < items.length; i++) {
+		alert(items[i].getText());
+	};
+
+	//lista item -- Jquery
+	for (var i = 0; i < items.length; i++) {
+		//alert($(items[i]).text());
+	};
+});
+
